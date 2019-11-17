205ef6b49394497909c8d8e57b5b570a4f104360
Philipp Schardt <philipp.schardt@ergosign.de>
2019-11-17 21:21:13 +0100

    register working

--- src/register/register.tsx
+++ src/register/register.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import addons, { types } from '@storybook/addons';
+import addons, { MatchOptions, types } from '@storybook/addons';
 import { PseudoStateTool } from '../share/PseudoStateTool';
 import { ADDON_ID, TOOL_ID, TOOL_TITLE } from '../share/constants';
 
 
-export default addons.register(ADDON_ID, (/*api: API*/): void => {
+export default () => {
 
-  addons.add(TOOL_ID, {
-    title: TOOL_TITLE,
-    type: types.TOOL,
-    match: ({viewMode}) => viewMode === 'story',
-    render: () => <PseudoStateTool /*api={api}*//>
+  addons.register(ADDON_ID, (/*api: API*/): void => {
+
+    addons.add(TOOL_ID, {
+      title: TOOL_TITLE,
+      type: types.TOOL,
+      match: ({viewMode}: MatchOptions) => viewMode === 'story',
+      render: () => <PseudoStateTool /*api={api}*//>
+    });
+
+    // addons.addPanel(PANEL_ID, {
+    //   title,
+    //   render: ({active, key}) => <PseudoStatePanel key={key}
+    //                                                api={api}
+    //                                                active={active}/>
+    // });
+
+    // addons.add(PREVIEW_ID, {
+    //     title: 'pseudo test',
+    //     type: types.PREVIEW,
+    //     match: ({ viewMode }) => viewMode === 'story',
+    //     render: () => <MyTool api={api} />,
+    // });
   });
 
-  // addons.addPanel(PANEL_ID, {
-  //   title,
-  //   render: ({active, key}) => <PseudoStatePanel key={key}
-  //                                                api={api}
-  //                                                active={active}/>
-  // });
-
-  // addons.add(PREVIEW_ID, {
-  //     title: 'pseudo test',
-  //     type: types.PREVIEW,
-  //     match: ({ viewMode }) => viewMode === 'story',
-  //     render: () => <MyTool api={api} />,
-  // });
-});
-
+}
