00a77f3fa2fb0ae2ee7df0cdc7a75e451c3769a1
Philipp Schardt <philipp.schardt@ergosign.de>
2019-11-20 20:05:07 +0100

    more bazel rollup tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
-
+import ts from "@wessberg/rollup-plugin-ts";
+import dts from "rollup-plugin-dts";
 
 module.exports = {
     output: {
     },
     plugins: [
+        ts({
+            /* Plugin options */
+            // tsconfig: "tsconfig.json"
+        })
+        // dts()
     ],
-    context: "window"
+    context: ["window", "react"]
 };
